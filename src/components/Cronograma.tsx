dfcf635e3620068c01272ebb1a2cd84f5abcc333
Ronald Oliveira <devrdonee@gmail.com>
2026-05-13 15:40:12 -0700

    fix: bug na variante na hora do deploy/2

--- src/components/Cronograma.tsx
+++ src/components/Cronograma.tsx
@@ -65,7 +65,7 @@ export function Cronograma() {
     }
   };
 
-  const itemVariants = {
+  const itemVariants: Variants = {
     hidden: { opacity: 0, y: 30 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } }
   };
